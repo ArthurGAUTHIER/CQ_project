b5edf446c471613cc54962598234d6e2b54ff42c
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-10-17 18:42:09 +0200

    done

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,2 +1,7 @@
 class Agent < ApplicationRecord
+  has_many :bookings, through: :agent_bookings
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :work_zone, presence: true
 end
